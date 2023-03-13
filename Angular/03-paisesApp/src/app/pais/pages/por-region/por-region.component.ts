a5efdac83d72cbd5bfe5100ecf47da08d5b72dbb
Moitas500 <camilofour1@gmail.com>
2023-03-13 11:26:09 -0500

    Region

--- Angular/03-paisesApp/src/app/pais/pages/por-region/por-region.component.ts
+++ Angular/03-paisesApp/src/app/pais/pages/por-region/por-region.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styleUrls: ['./por-region.component.css']
+  styleUrls: ['./por-region.component.css'],
+  styles: [
+    `
+    button{
+      margin-right: 5px
+    }
+    `
+  ]
 })
 export class PorRegionComponent {
+
+  regiones: string[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  regionActiva: string = "";
+
+  activarRegion( region:string ){
+    this.regionActiva = region;
+
+
+  }
+
+
 
 }
